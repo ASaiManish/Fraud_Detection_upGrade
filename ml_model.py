56ee23dddabd4f297eeb401d454d80ea596d0f53
ASaiManish <saimanish07@gmail.com>
2023-05-27 20:51:03 +0530

    Added 3 models code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,10 @@
 print('Adding all ML related code here')
+
+def model1():
+    print("this is model1")
+
+def model2():
+    print('This is model 2')
+
+def model3():
+    print('This has code for model 3')
